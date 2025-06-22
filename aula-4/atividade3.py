5ca7c967000c9cf88ec37f6044ae1bc58b344f1c
victor <victor.h.gutierrez.carvalho>
2025-06-21 22:44:16 -0300

    calculo do IMC

--- aula-4/atividade3.py
+++ aula-4/atividade3.py
@@ -3,3 +3,10 @@ altura = float(input("Coloque a sua altura: "))
 
 imc = peso / altura**2
 print("Seu IMC: ", imc)
+
+if imc < 18.6: print("Abaixo do peso")
+elif imc < 24.9: print("Peso ideal")
+elif imc < 29.9: print("Sobre-peso")
+elif imc < 34.9: print("Obessidade grau 1")
+elif imc < 39.9: print("Obessidade grau 2")
+else: print("Obessidade grau 3")
