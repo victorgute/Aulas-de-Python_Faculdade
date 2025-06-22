3c1a7a13105b1b36065f000c593db35a0d713a47
victor <victor.h.gutierrez.carvalho>
2025-06-22 08:04:21 -0300

    calculo para anos bissextos e dias do mes

---
+++ aula-4/atividade7.py
@@ -0,0 +1,14 @@
+mes = int(input("Insira um mes: "))
+ano = int(input("Insira um ano:"))
+
+if mes <1 or mes >12: print("Mês invalido")
+else:
+    if ano % 100 == 0 and ano % 400 == 0 or ano % 4 == 0 and ano % 100 != 0: biseexto = 0
+    else: biseexto = 1
+    if mes == 2 and biseexto == 0: dias = 29
+    elif mes == 2 and biseexto == 1: dias = 28
+    else:
+        if mes  == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12: dias = 31
+        else: dias = 30
+
+print(f"Mês {mes} do anos {ano} tem {dias} dias")
