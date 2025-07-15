bd4adfac40b4f1a5944d2d1340c656d6238d7035
Victor H. Gutierrez <164099865+victorgute@users.noreply.github.com>
2025-07-15 17:18:27 -0300

    Update exemplo29.py

---
+++ aula-5/exemplo29.py
@@ -0,0 +1,16 @@
+import random as rd
+
+sorteio = rd.randint(1,100)
+acerto = False
+
+numero = print("Você tem 10 chances para acertar o número sorteado")
+for resposta in range(1,10+1):
+    tentativa = int(input(f"Tentativa: {resposta} Escolha um numero de 1 a 100: "))
+    if tentativa > sorteio: print("Seu numero é maior que o numero sorteado, tente denovo")
+    if tentativa < sorteio: print("Seu numero é menor que o numero sorteado, tente denovo")
+    if tentativa == sorteio: acerto = True
+    if acerto == True:
+        break
+
+if acerto == True: print("Você acertou o numero sorteado, parabéns")
+if acerto == False: print(f"Acabou suas tentativas, esse era o numero sorteado: {sorteio}")
